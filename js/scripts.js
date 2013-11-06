8b1641f067760104a50dd92c081d8749f7a09a71
Raby <jraby92@engr-134-05.eastern.ewu.edu>
2013-11-06 14:57:15 -0800

    Version 2

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,39 @@
 
 $(document).ready(function(){
 	
-	// Good Stuff
+	// Switch to Grid View
+	$('#grid').click(function(){
+		
+		// 1. Hide Paragraphs
+		$('p').hide();
+		
+		// 2. Remove exsisting class
+		$('section').removeClass('col-md-6');
+		
+		// 3. Add new class
+		
+		$('section').addClass('col-md-3');
+		
+		
+		});
 	
+	
+		// Switch to List View
+	$('#list').click(function(){
+		
+		// 1. Show Paragraphs
+		$('p').show();
+		
+		// 2. Remove exsisting class
+		$('section').removeClass('col-md-3');
+		
+		// 3. Add new class
+		
+		$('section').addClass('col-md-6');
+		
+		
+		
+		
+		
+		});
 });
